80f8261b0719df163c026f9c81d5a47d919dce28
Ömer Faruk COŞGUN <mer Faruk COGUN@DESKTOP-BEK3OHO>
2021-09-17 15:44:42 +0300

    Add project files.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GithubTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GithubTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ac61b25d-e58b-4d55-b80a-1fa9845493ff")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest1.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
+
+namespace GithubTest
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+			ChromeDriverService service = ChromeDriverService.CreateDefaultService();
+			service.HideCommandPromptWindow = true;
+
+
+			IWebDriver driver = new ChromeDriver(service);
+			driver.Navigate().GoToUrl("https://github.com/");
+
+
+			IWebElement element = driver.FindElement(By.Name("q"));
+			element.SendKeys("aycayakicii");
+			Thread.Sleep(2000);
+			element.SendKeys(Keys.Enter);
+			
+
+
+			
+		}
+	}
+}
